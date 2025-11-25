b2488944e73eee3f1caa86bdee7317700b414bb8
Gabe Harris <gabekh200@gmail.com>
2025-11-25 15:16:24 -0500

    spencer for you love you <23 <2

--- penny_part_2.py
+++ penny_part_2.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.metrics import mean_squared_error
 
-df = pd.read_excel('datasets/penny_part_2/fed_diary_of_consumer_payment_choice/2025-diary-of-consumer-payment-choice-charts.xlsx', sheet_name='Figure 4')
+df = pd.read_excel('datasets/penny_part_2/fed_diary_of_consumer_payment_choice/2025-diary-of-consumer-payment-choice-charts.xlsx',
+                   sheet_name='Figure 4')
 df.rename(columns={'Unnamed: 0': 'Years'}, inplace=True)
 df['Years'] = pd.to_datetime(df['Years'], format='%Y')
 months = df.loc[:len(df)-2, ['Years','Cash']].copy()
 df['Cash'] = df['Cash']*100
 
-# trying lin reg for more data points - not useful
+# lin reg for more data points to avoid low data errors - not advisable!
 months_list = []
 for i in range(len(df) - 1):
     slope = (df.loc[i + 1, 'Cash'] - df.loc[i, 'Cash']) / 12
@@ -20,24 +21,24 @@ for i in range(len(df) - 1):
         months_list.append({'Date': current_date, 'Cash': current_cash})
 months_df = pd.DataFrame(months_list)
 cash_df = months_df[['Date','Cash']]
-print(months_df)
-print(cash_df)
 
-p,d,q = 2,2,1
+p,d,q = 2,1,5
 model = SARIMAX(cash_df['Cash'], seasonal_order=(p,d,q,12))
 model_fit = model.fit()
 prediction = model_fit.forecast(steps=len(cash_df))
 mse = mean_squared_error(cash_df['Cash'], prediction)
 
-forecast = model_fit.forecast(steps=48)
+future_months = 120
+forecast = model_fit.forecast(steps=future_months)
+
 last_year = cash_df['Date'].dt.year.iloc[-1]
-future_years = pd.date_range(start=f'{last_year+1}', periods=48, freq='ME')
+future_years = pd.date_range(start=f'{last_year+1}', periods=future_months, freq='ME')
 
 # Combine for plotting
 forecast_df = pd.DataFrame({'Cash': forecast.values}, index=future_years)
 
 plt.plot(cash_df['Date'], cash_df['Cash'], label="Known Values")
-plt.plot(forecast_df.index, forecast_df['Cash'], label="Forecast", linestyle='--', color='red')
+plt.plot(forecast_df.index, forecast_df['Cash'], label="Forecast", linestyle='-', color='red')
 plt.xlabel("Date")
 plt.ylabel("Transactions in Cash (%)")
 plt.legend()
