85454bf0ce8eb8766ab678e4394943c89c8b43fb
Gabe Harris <gabekh200@gmail.com>
2025-11-25 14:49:38 -0500

    sarima working

--- penny_part_2.py
+++ penny_part_2.py
@@ -6,32 +6,40 @@ from sklearn.metrics import mean_squared_error
 df = pd.read_excel('datasets/penny_part_2/fed_diary_of_consumer_payment_choice/2025-diary-of-consumer-payment-choice-charts.xlsx', sheet_name='Figure 4')
 df.rename(columns={'Unnamed: 0': 'Years'}, inplace=True)
 df['Years'] = pd.to_datetime(df['Years'], format='%Y')
+months = df.loc[:len(df)-2, ['Years','Cash']].copy()
 df['Cash'] = df['Cash']*100
-cash_df = df[['Years','Cash']]
 
-p,d,q = 0,0,0
-best_mse = 10000
-best_set = (p,d,q)
-for p in range(0,3):
-    for d in range(0,3):
-        for q in range(0,3):
-            model = SARIMAX(df['Cash'], order=(p,d,q))
-            model_fit = model.fit()
-            prediction = model_fit.forecast(steps=len(cash_df))
-            mse = mean_squared_error(cash_df['Cash'], prediction)
-            if mse < best_mse:
-                best_mse = mse
-                best_set = (p,d,q)
-                best_model = model
-                best_prediction = prediction
-print(best_set)
+# trying lin reg for more data points - not useful
+months_list = []
+for i in range(len(df) - 1):
+    slope = (df.loc[i + 1, 'Cash'] - df.loc[i, 'Cash']) / 12
+    y_int = df.loc[i, 'Cash']
+    for n in range(12):
+        current_date = df.loc[i, 'Years'] + pd.DateOffset(months=n)
+        current_cash = y_int + slope * n
+        months_list.append({'Date': current_date, 'Cash': current_cash})
+months_df = pd.DataFrame(months_list)
+cash_df = months_df[['Date','Cash']]
+print(months_df)
+print(cash_df)
 
-plt.plot(cash_df['Years'], cash_df['Cash'], label="Known Values")
-plt.xlabel("Year")
+p,d,q = 2,2,1
+model = SARIMAX(cash_df['Cash'], seasonal_order=(p,d,q,12))
+model_fit = model.fit()
+prediction = model_fit.forecast(steps=len(cash_df))
+mse = mean_squared_error(cash_df['Cash'], prediction)
+
+forecast = model_fit.forecast(steps=48)
+last_year = cash_df['Date'].dt.year.iloc[-1]
+future_years = pd.date_range(start=f'{last_year+1}', periods=48, freq='ME')
+
+# Combine for plotting
+forecast_df = pd.DataFrame({'Cash': forecast.values}, index=future_years)
+
+plt.plot(cash_df['Date'], cash_df['Cash'], label="Known Values")
+plt.plot(forecast_df.index, forecast_df['Cash'], label="Forecast", linestyle='--', color='red')
+plt.xlabel("Date")
 plt.ylabel("Transactions in Cash (%)")
-plt.xticks(rotation=45)
-
-# Grid/Auto-Manual Fit ARIMA w/ box-cox
 plt.legend()
 plt.title("ARIMA Prediction")
 plt.show()
